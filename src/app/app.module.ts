8c3e5be50d59184407fdd5e20f024800d30c27e1
Garrett Vangilder <garrett.vangilder@gmail.com>
2017-04-17 07:43:27 -0500

    added map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Geolocation } from '@ionic-native/geolocation';
+import { AgmCoreModule } from 'angular2-google-maps/core';
+import { APIKeys } from './keys.ts';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -14,7 +17,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AgmCoreModule.forRoot({
+      apiKey: APIKeys.googleMaps
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,6 +30,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, ToastController } from 'ionic-angular';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  locationInfo: Geoposition;
+  locationIsSet: boolean = false;
+  constructor(public navCtrl: NavController,
+              private loadingCtrl: LoadingController,
+              private geolocation: Geolocation,
+              private toastController: ToastController) {
+  }
 
-  constructor(public navCtrl: NavController) {
+  ionViewWillEnter() {
+    this.getLocation()
+  }
 
+  private getLocation() {
+    const loader = this.loadingCtrl.create({
+      content: 'Getting your location...'
+    })
+    loader.present();
+       this.geolocation.getCurrentPosition()
+      .then(
+        location => {
+          loader.dismiss()
+          this.locationInfo = location;
+          this.locationIsSet = true;
+          console.log(this.locationInfo);
+        }
+      )
+      .catch(
+        error => {
+          const toast = this.toastController.create({
+            message: 'Could not get location, please search manually',
+            duration: 2500
+          })
+          toast.present();
+        }
+      );
   }
 
 }
